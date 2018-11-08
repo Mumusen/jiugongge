5943704e40bc9233a18b2d424dcf3d693fe2f34b
linxiao <cylinxiao@163.com>
2018-11-09 00:40:44 +0800

    修复定时器

--- style.js
+++ style.js
@@ -1,54 +1,55 @@
-  var spans = document.getElementsByTagName('span');
-  var colors = ['#FFFAFA', '#FFB90F', '#FF1493', '#EEEEE0', '#D1EEEE', '#B5B5B5', '#76EE00', '#27408B', '#00FFFF'];
+var spans = document.getElementsByTagName('span');
+var colors = ['#FFFAFA', '#FFB90F', '#FF1493', '#EEEEE0', '#D1EEEE', '#B5B5B5', '#76EE00', '#27408B', '#00FFFF'];
+var time;
+function startClick() {
+  time && clearInterval(time);
+  time = setInterval(function () {
+    start();
+  }, 1000);
+};
 
-  function startClick() {
-    time = setInterval(function () {
-      start();
-    }, 1000);
+function stop() {
+  clearInterval(time);
+  for (var i = 0; i < spans.length; ++i) {
+    spans[i].style.background = "#fda259";
   };
+}
 
-  function stop() {
-    clearInterval(time);
-    for (var i = 0; i < spans.length; ++i) {
-      spans[i].style.background = "aquamarine";
-    };
-  }
-
-  function start() {
-    for (var i = 0; i < spans.length; ++i) {
-      spans[i].style.background = "aquamarine";
-    };
-    var number = new Array(3);
-    var arr = new Array(3);
-    for (var i = 0; i < number.length; ++i) {
-      var a = parseInt(Math.random() * 9);
-      if (i === 0) {
-        number[i] = a;
-      } else {
-        for (var j = 0; j < i; ++j) {
-          if (a === number[j]) {
-            i--;
-          } else {
-            number[i] = a;
-          };
+function start() {
+  for (var i = 0; i < spans.length; ++i) {
+    spans[i].style.background = "#fda259";
+  };
+  var number = new Array(3);
+  var arr = new Array(3);
+  for (var i = 0; i < number.length; ++i) {
+    var a = parseInt(Math.random() * 9);
+    if (i === 0) {
+      number[i] = a;
+    } else {
+      for (var j = 0; j < i; ++j) {
+        if (a === number[j]) {
+          i--;
+        } else {
+          number[i] = a;
         };
       };
     };
-    for (var i = 0; i < arr.length; ++i) {
-      var a = parseInt(Math.random() * 9);
-      if (i === 0) {
-        arr[i] = a;
-      } else {
-        for (var j = 0; j < i; ++j) {
-          if (a === arr[j]) {
-            i--;
-          } else {
-            arr[i] = a;
-          };
+  };
+  for (var i = 0; i < arr.length; ++i) {
+    var a = parseInt(Math.random() * 9);
+    if (i === 0) {
+      arr[i] = a;
+    } else {
+      for (var j = 0; j < i; ++j) {
+        if (a === arr[j]) {
+          i--;
+        } else {
+          arr[i] = a;
         };
       };
     };
-    for (var i = 0; i < number.length; ++i) {
-      spans[number[i]].style.background = colors[arr[i]];
-    };
   };
+  for (var i = 0; i < number.length; ++i) {
+    spans[number[i]].style.background = colors[arr[i]];
+  };
+};
